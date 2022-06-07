4306677506477ddeb17452cd8f1b2358c8b61d80
Mariana Gomes <88630647+marianasgit@users.noreply.github.com>
2022-06-07 11:53:21 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,156 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste API</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+require_once('vendor/autoload.php');
+
+$app = new \Slim\app();
+
+// Endpoint para LISTAR todos os registros
+$app -> get('/registros', function($request, $response, $args)
+{
+    require_once('../modulo/config.php');
+    require_once('../controller/registrosController.php');
+
+    if ($dados = listarRegistros())
+    {
+        if ($dadosJSON = createJSON($dados))
+        {
+            return $response    -> withStatus(200)
+                                -> withHeader('Content-Type', 'application/json')
+                                -> write($dadosJSON);
+
+
+        }
+    } else
+    {
+        return $response    -> withStatus(404)
+                            -> withHeader('Content-Type', 'application/json')
+                            -> write('{"message" : "Item não encontrado"}');
+                    
+    }
+});
+
+// Endpoint para listar apenas um registro
+$app -> get('/registros/{id}', function($request, $response, $args)
+{
+    $id = $args['id'];
+
+    require_once('../modulo/config.php');
+    require_once('../controller/registrosController.php');
+
+    if ($dados = buscarRegistro($id))
+    {
+        if (!isset($dados['idErro']))
+        {
+            if ($dadosJSON = createJSON($dados))
+            {
+                return $response    -> withStatus(200)
+                                    -> withHeader('Content-Type', 'application/json')
+                                    -> write($dadosJSON);
+            }
+        } else
+        {
+            $dadosJSON = createJSON($dados);
+            
+            return $response    -> withStatus(404) 
+                                -> withHeader('Content-Type', 'application/json')
+                                -> write('{"message" : "Dados Inválidos", "Erro" : '.$dadosJSON.'}');
+
+        } 
+    } else
+        {
+            return $response    -> withStatus(404)
+                                -> withHeader('Content-Type', 'application/json')
+                                -> write('{"message" : "Item não encontrado"}');
+        }
+});
+
+// Endpoint para inserir um registro
+$app -> post('/registros', function($request, $response, $args)
+{
+    $contentTypeHeader = $request -> getHeaderLine('Content-Type');
+
+    $contentType = explode(";", $contentTypeHeader);
+
+    switch ($contentType[0])
+    {
+        case 'multipart/form-data' :
+
+            $dadosBody = $request -> getParsedBody();
+
+            $arrayDados = array ($dadosBody);
+
+            require_once('../modulo/config.php');
+            require_once('../controller/registrosController.php');
+
+            $resposta = inserirRegistro($arrayDados);
+
+            if (is_bool($resposta) && $resposta == true)
+            {
+                return $response -> withStatus(201)
+                                    -> withHeader('Content-Type', 'application/json')
+                                    -> write('{"message" : "Registro inserido com sucesso"}');
+
+
+            } elseif (is_array($resposta) && $resposta['idErro'])
+            {
+                $dadosJSON = createJSON($resposta);
+
+                return $response    -> withStatus(404)
+                                    -> withHeader('Content-Type', 'application/josn')
+                                    -> write('{"message" : "Houve um problema no processo de inserção", "Erro" : '.$dadosJSON.'}');
+            }
+
+            break;
+
+        case 'application/json' :
+            
+            return $response    -> withStatus(200)
+                                -> withHeader('Content-Type', 'application/json')
+                                -> write('{"message" : "Formato selecionado foi JSON"}');
+
+            break;
+            
+        default :
+            return $response    -> withStatus(400)
+                                -> withHeader('Content-Type', 'application/json')
+                                -> write('{"message" : "Formato selecionado inválido"}');
+
+            break;                        
+
+
+    }
+});
+
+// Endpoint para deletar registro
+$app -> delete('/contatos/{id}', function($request, $response, $args)
+{
+    if (is_numeric($args['id']))
+    {
+        $id = $args['id'];
+
+        require_once('../modulo/config.php');
+        require_once('../controller/registrosController.php');
+
+        if ($dados = buscarRegistro($id))
+        {
+            $arrayDados = array("id" => $id);
+
+            $resposta = excluirRegistro($arrayDados);
+
+            if (is_bool($resposta) && $resposta == true)
+            {
+                return $response    -> withStatus(200)
+                                    -> withHeader('Content-Type', 'application/json')
+                                    -> write('{"message" : "Registro excluído com sucesso"}');
+
+            } else 
+            {
+                return $response    -> withStatus(404)
+                                    -> withHeader('Content-Type', 'application/json')
+                                    -> write('{"message" : "O Id informado não foi encontrado"}')
+            }
+        }
+    }
+})
+
+?>
--- registros.php
+++ registros.php
@@ -12,7 +12,7 @@
 
         $result = mysqli_query($conexao, $sql);
 
-        if (result)
+        if ($result)
         {
             $cont = 0;
 
@@ -20,14 +20,132 @@
             {
                 $arrayDados[$cont] = array(
                     "id"                => $rsDados['id'],            
-                    "nome_cliente"      => $rsDados['nome_cliente'],
-                    "placa_veiculo"     => $rsDados     
-                    "modelo_veiculo"    
-                    "entrada"
-                    "saida"
-                    "valor_total"
-                )
+                    "nome_cliente"      => $rsDados['cliente'],
+                    "placa_veiculo"     => $rsDados['placa'],     
+                    "modelo_veiculo"    => $rsDados['modelo'],
+                    "entrada"           => $rsDados['entrada'],
+                    "saida"             => $rsDados['saida'],
+                    "valor_total"       => $rsDados['total']
+                );
+
+                $cont++;
             }
+
+            fecharConexaoMySql($conexao);
+
+            if (isset($arrayDados))
+            {
+                return $arrayDados;
+            } else 
+            {
+                return false;
+            }
+        }
+    }
+
+    function insertRegistro($dadosRegistro)
+    {
+        $conexao = conexaoMySql();
+
+        $sql = "insert into registros
+                            (nome_cliente,
+                             placa_veiculo,
+                             modelo_veiculo,
+                             entrada
+                             )
+                    values
+                        ('" . $dadosRegistro['nome'] ."',
+                        '" . $dadosRegistro['placa'] ."',
+                        '" . $dadosRegistro['modelo'] ."',
+                        sysdate);";
+
+        if (mysqli_query($conexao, $sql))
+        {
+            if (mysqli_affected_rows($conexao))
+            {
+                $statusResposta = true;
+            }
+        } else
+        {
+            fecharConexaoMySql($conexao);
+        }                
+        return $statusResposta;
+    }
+
+    function updateRegistro($dadosRegistro)
+    {
+        $conexao = conexaoMySql();
+
+        $sql = "update registros set
+                        nome_cliente = '". $dadosRegistro['nome'] ."',
+                        placa_veiculo = '". $dadosRegistro['placa'] ."',
+                        modelo_veiculo = '". $dadosRegistro['veiculo'] ."',
+                        nome_cliente = '". $dadosRegistro['entrada'] ."'
+                    where id =" .$dadosRegistro['id'];
+
+        if (mysqli_query($conexao, $sql))
+        {
+            if (mysqli_affected_rows($conexao))
+            {
+                $statusResposta = true;
+            }
+        } else 
+        {
+            fecharConexaoMySql($conexao);
+        }            
+    }
+
+    function deleteRegistro($id)
+    {
+        $conexao = conexaoMySql();
+
+        $sql = "delete from registros where id = " .$id;
+
+        if (mysqli_query($conexao, $sql))
+        {
+            if (mysqli_affected_rows($conexao))
+            {
+                $statusResposta = true;
+            }
+        }
+
+        fecharConexaoMySql($conexao);
+
+        return $statusResposta;
+    }
+
+    function selectByIdRegistro($id)
+    {
+        $conexao = conexaoMySql();
+
+        $sql = "select * from registros where id = " .$id;
+
+        $result = mysqli_query($conexao, $sql);
+
+        if ($result)
+        {
+            if ($rsDados = mysqli_fetch_assoc($result))
+            {
+                $arrayDados = array(
+                    "id"                => $rsDados['id'],
+                    "nome_cliente"      => $rsDados['nome'],
+                    "placa_veiculo"     => $rsDados['placa'],
+                    "modelo_veiculo"    => $rsDados['modelo'],
+                    "entrada"           => $rsDados['entrada'],
+                    "saida"             => $rsDados['saida'],
+
+                );
+            }
+        }
+
+        fecharConexaoMySql($conexao);
+
+        if(isset($arrayDados))
+        {
+            return $arrayDados;
+        } else 
+        {
+            return false;
         }
     }
 ?>
